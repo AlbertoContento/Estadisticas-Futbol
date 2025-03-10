1f462f52f786c54b0023640d723a098905a46324
Alberto <albertillo_ec@hotmail.com>
2025-03-10 18:46:22 +0100

    Proyecto funcionando hasta la importacion de datos de los .csv

--- equipos/admin.py
+++ equipos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from equipos.models import Equipo
+from equipos.models import EstadisticasEquipo
 
-# Register your models here.
+@admin.register(Equipo)
+class EquiposAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'liga', 'logo')
+
+@admin.register(EstadisticasEquipo)
+class EestadisticasEquipoAdmin(admin.ModelAdmin):
+    list_display = ('equipo', 'partidos_jugados', 'partidos_ganados', 'partidos_empatados', 'partidos_perdidos', 'goles_a_favor', 'goles_en_contra', 'diferencia_goles', 'puntos', 'asistencias', 'penaltis_marcados', 'tarjetas_amarillas', 'tarjetas_rojas', 'media_posesion', 'ultimos_5_partidos', 'maximo_goleador')
--- equipos/models.py
+++ equipos/models.py
@@ -1,23 +1,35 @@
 from django.db import models
 from ligas.models import Liga
 
-# Create your models here.
+#Modelo Equipo
 class Equipo(models.Model):
   nombre = models.CharField(max_length=50, unique=True)
   liga = models.ForeignKey(Liga, on_delete=models.CASCADE, related_name='equipos')#si se borra una liga se borran los equipos
   logo = models.ImageField(upload_to='logos_equipos/', blank=True, null=True)
+  
+  def __str__(self):
+    return self.nombre
+  
 
   # 📊 Estadísticas generales del equipo
-  goles_marcados = models.PositiveIntegerField(default=0)
-  goles_contra = models.PositiveIntegerField(default=0)
+class EstadisticasEquipo(models.Model):
+  equipo = models.OneToOneField(Equipo, on_delete=models.CASCADE, related_name='estadisticas')
+  partidos_jugados = models.PositiveIntegerField(default=0)
+  partidos_ganados = models.PositiveIntegerField(default=0)
+  partidos_empatados = models.PositiveIntegerField(default=0)
+  partidos_perdidos = models.PositiveIntegerField(default=0)
+  goles_a_favor = models.PositiveIntegerField(default=0)
+  goles_en_contra = models.PositiveIntegerField(default=0)
+  diferencia_goles = models.IntegerField(default=0)
+  puntos = models.PositiveIntegerField(default=0)
   asistencias = models.PositiveBigIntegerField(default=0)
   penaltis_marcados = models.PositiveBigIntegerField(default=0)
   tarjetas_amarillas = models.PositiveBigIntegerField(default=0)
   tarjetas_rojas= models.PositiveBigIntegerField(default=0)
   media_posesion = models.FloatField(default=50.0)  # Porcentaje medio de posesión (ej. 55.3)
-  partidos_jugados = models.PositiveBigIntegerField(default=0)
-  diferencia_goles = models.IntegerField(default=0)
-  ultimo_partido = models.PositiveBigIntegerField(default=0)
+  ultimos_5_partidos = models.CharField(max_length=10, default='')
+  maximo_goleador = models.CharField(max_length=255, default='')
+
 
   def __str__(self):
-    return self.nombre
+    return f"Estadísticas de {self.equipo.nombre}"
