ec7129a13c449addc81876aa589a296398cd07c6
Alberto Contento Guerrero <albertocontentoguerrero@hotmail.com>
2025-03-17 18:38:17 +0100

    Update views.py

--- ligas/views.py
+++ ligas/views.py
@@ -1,25 +1,15 @@
 from django.shortcuts import render
 from django.views.generic import ListView, DetailView
 from .models import Liga
-<<<<<<< HEAD
-
-
-class LigaListView(ListView):
-=======
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 
 class LigaListView(LoginRequiredMixin, ListView):
->>>>>>> recuperacion
   model = Liga #Modelo
   template_name = 'ligas/ligas_list.html' # Nombre Plantilla
   context_object_name = 'ligas' # nombre variable en el template
 
-<<<<<<< HEAD
-class LigaDetailView(DetailView):
-=======
 class LigaDetailView(LoginRequiredMixin, DetailView):
->>>>>>> recuperacion
   model = Liga #Modelo
   template_name = 'ligas/liga_detail.html' # Nombre Plantilla
   context_object_name = 'liga' # nombre variable en el template
